02cc779d77151a69920870f56cb6d9574e60837d
Anudeepkumar7 <125281933+Anudeepkumar7@users.noreply.github.com>
2024-08-21 15:19:27 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket         = "my-aws-bucket11"
     region         = "ap-south-1"
-    key            = "eks/terraform/"
+    //key            = "eks/terraform/"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
